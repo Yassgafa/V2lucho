9130e9e813222caa8c6ae289e3d9dc282edc9a53
Yassgafa <yassirfalco@hotmail.com>
2024-02-07 16:25:46 -0500

    first commit

---
+++ app/src/main/java/co/edu/funlam/v2luchitokotlin/MainActivity.kt
@@ -0,0 +1,46 @@
+package co.edu.funlam.v2luchitokotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.funlam.v2luchitokotlin.ui.theme.V2LuchitoKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            V2LuchitoKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    V2LuchitoKotlinTheme {
+        Greeting("Android")
+    }
+}
